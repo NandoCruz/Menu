a9ee1dd08b8c90af2d5a8c40faa52ed0c3d90f51
Fernando Cruz <nando33_2005@hotmail.com>
2025-03-13 17:51:52 -0300

    Atualizando slides css
    Ajustando o slides css

--- slide.js
+++ slide.js
@@ -1,5 +1,12 @@
 let radio = document.querySelector('.manual-btn')
-let cont = 1
+let btnRadio = document.getElementById('.manual-btn')
+let containerScroll = document.querySelector('.container-scroll')
+let item = containerScroll.querySelectorAll('.nav-auto .auto-btn')
+
+let active = 0
+let position = 1
+let firtPosition = 1
+let lastPosition = item.length -1
 
 document.getElementById('radio1').checked = true
 
@@ -8,12 +15,18 @@ setInterval(() => {
 }, 3000)
 
 function proximaImg(){
-    cont++
 
-    if(cont > 5){
-        cont = 1
+    position++
+    
+    if(position > 5){
+        position = 1
     }
 
-    document.getElementById('radio'+cont).checked = true
+    document.getElementById('radio' + position).checked = true
+    
+    active = active + 1 > lastPosition ? 0 : active +1
 
+    let itemOld = containerScroll.querySelector('.nav-auto .auto-btn.active')
+    itemOld.classList.remove('active')
+    item[active].classList.add('active')
 }

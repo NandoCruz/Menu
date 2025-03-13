6ecc78e987031137d6414439dd98cfc4780b8948
Fernando Cruz <nando33_2005@hotmail.com>
2025-03-13 10:21:28 -0300

    Ajustando o Scroll
    Atualizando o scroll dos cartões

---
+++ slide.js
@@ -0,0 +1,19 @@
+let radio = document.querySelector('.manual-btn')
+let cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 3000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 5){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+
+}

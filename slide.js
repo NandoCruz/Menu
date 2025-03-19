dd135d1b532f16033c10cdb46f5bbff85cf73cf4
Fernando Cruz <nando33_2005@hotmail.com>
2025-03-19 11:37:34 -0300

    Atualizando CSS

--- slide.js
+++ slide.js
@@ -2,10 +2,11 @@ let radio = document.querySelector('.manual-btn')
 let btnRadio = document.getElementById('.manual-btn')
 let containerScroll = document.querySelector('.container-scroll')
 let item = containerScroll.querySelectorAll('.nav-auto .auto-btn')
+let container = containerScroll.querySelectorAll('.container-master .container')
 
+let cont = 1
 let active = 0
-let position = 1
-let firtPosition = 1
+let firtPosition = 0
 let lastPosition = item.length -1
 
 document.getElementById('radio1').checked = true
@@ -16,17 +17,21 @@ setInterval(() => {
 
 function proximaImg(){
 
-    position++
+    cont++
     
-    if(position > 5){
-        position = 1
+    if(cont > 5){
+        cont = 1
     }
 
-    document.getElementById('radio' + position).checked = true
+    document.getElementById('radio' + cont).checked = true
     
     active = active + 1 > lastPosition ? 0 : active +1
 
     let itemOld = containerScroll.querySelector('.nav-auto .auto-btn.active')
     itemOld.classList.remove('active')
     item[active].classList.add('active')
+
+    let containerOld = containerScroll.querySelector('.container-master .container.active')
+    containerOld.classList.remove('active')
+    container[active].classList.add('active')
 }
